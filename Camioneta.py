2cdc9f4158c5eb198e1f30cc19022de81eb0ab5c
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2023-03-02 09:32:07 +0100

    clase camioneta

---
+++ Camioneta.py
@@ -0,0 +1,9 @@
+import Vehiculo
+import Coche
+
+class Camioneta(Coche):
+    def __init__(self, color, ruedas, velocidad, cilindrada, carga):
+        Coche.__init__(self, color, ruedas, velocidad, cilindrada)
+        self.carga = carga
+    def __str__(self):
+        return Coche.__str__(self) + ", {} kg".format(self.carga)
