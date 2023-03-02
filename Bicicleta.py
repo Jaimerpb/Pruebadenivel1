607c71976fee0acb514251837412854908a144ca
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2023-03-02 09:37:01 +0100

    clase bicicleta

---
+++ Bicicleta.py
@@ -0,0 +1,8 @@
+import Vehiculo 
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        Vehiculo.__init__(self, color, ruedas)
+        self.tipo = tipo
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {}".format(self.tipo)
+
