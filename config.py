263cc9425f2291f040e9dbaf8b647df0955386f3
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2023-03-07 01:17:59 +0100

    fas

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import sys
 
-DATABASE_PATH = "vehiculos.csv"
+DATABASE_PATH = "Vehiculos.csv"
 
 if "pytest" in sys.argv[0]:
-    DATABASE_PATH = "tests/clientes_test.csv"
+    DATABASE_PATH = "test/vehiculos_test.csv"
